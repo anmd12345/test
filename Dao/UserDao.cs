72d326d9575b9e72a48c6f0c63c4c298549eb7c3
anmd12345 <anvohoang98@gmail.com>
2024-04-18 16:39:22 +0700

    add function getUserById

---
+++ Dao/UserDao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestGithub.Dao
+{
+    public class UserDao
+    {
+        private ConectionDataContext db = new ConectionDataContext();
+
+        public user getUserById (int id)
+        {
+            return db.users.FirstOrDefault(user => user.id == id);
+        }
+    }
+}
